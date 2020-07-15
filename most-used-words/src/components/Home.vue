7045c8bb850dcb2438089428ac9255e4d5ff5844
Wagner <wg.o.costa@gmail.com>
2020-07-14 23:20:57 -0300

    Criando a interface gráfica do projeto

--- most-used-words/src/components/Home.vue
+++ most-used-words/src/components/Home.vue
@@ -1,11 +1,71 @@
 <template>
-  <div>
-    Home
-  </div>
+  <v-container fluid>
+    <div class="pills">
+      <Pill
+        v-for="word in groupedWords"
+        :key="word.name"
+        :name="word.name"
+        :amount="word.amount"
+      />
+    </div>
+  </v-container>
 </template>
 
 <script>
-export default {};
+import Pill from "./Pill";
+export default {
+  components: {
+    Pill,
+  },
+  data: function() {
+    return {
+      groupedWords: [
+        { name: "you", amount: 900 },
+        { name: "i", amount: 950 },
+        { name: "he", amount: 1700 },
+        { name: "wg", amount: 1 },
+        { name: "you", amount: 900 },
+        { name: "i", amount: 950 },
+        { name: "he", amount: 1700 },
+        { name: "wg", amount: 1 },
+        { name: "you", amount: 900 },
+        { name: "i", amount: 950 },
+        { name: "he", amount: 1700 },
+        { name: "wg", amount: 1 },
+        { name: "you", amount: 900 },
+        { name: "i", amount: 950 },
+        { name: "he", amount: 1700 },
+        { name: "wg", amount: 1 },
+        { name: "you", amount: 900 },
+        { name: "i", amount: 950 },
+        { name: "he", amount: 1700 },
+        { name: "wg", amount: 1 },
+        { name: "you", amount: 900 },
+        { name: "i", amount: 950 },
+        { name: "he", amount: 1700 },
+        { name: "wg", amount: 1 },
+        { name: "you", amount: 900 },
+        { name: "i", amount: 950 },
+        { name: "he", amount: 1700 },
+        { name: "wg", amount: 1 },
+        { name: "you", amount: 900 },
+        { name: "i", amount: 950 },
+        { name: "he", amount: 1700 },
+        { name: "wg", amount: 1 },
+        { name: "you", amount: 900 },
+        { name: "i", amount: 950 },
+        { name: "he", amount: 1700 },
+        { name: "wg", amount: 1 },
+      ],
+    };
+  },
+};
 </script>
 
-<style></style>
+<style>
+.pills {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
---
+++ most-used-words/src/components/Pill.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="pill">
+    <span class="name">{{ name }}</span>
+    <span class="amount">{{ amount }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "amount"],
+};
+</script>
+
+<style>
+.pill {
+  background-color: crimson;
+  color: #fff;
+  margin: 10px;
+  height: 55px;
+  border-radius: 30px;
+  font-size: 1.3rem;
+  font-weight: bold;
+
+  display: flex;
+  align-items: center;
+}
+
+.pill .name {
+  margin-left: 15px;
+}
+
+.pill .amount {
+  margin: 10px;
+  padding: 5px;
+  height: 35px;
+  widows: 35px;
+  border-radius: 18px;
+  background-color: #fff;
+  color: crimson;
+  font-size: 14px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
